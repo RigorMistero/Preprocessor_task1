a36d1f9e0c18dd613d1358a3d1b12aef9c2402bd
Rigor Mistero <egor@student.su>
2025-09-07 18:15:47 +0300

    first commit

---
+++ Preprocessor_task1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+#define MODE '1'
+
+int add(int a, int b);
+
+int main(int argc, char** argv)
+{
+//	std::cout << "Fuck you!" << std::endl;
+#ifndef MODE
+#error DEFINE MODE
+#endif 
+	
+#ifdef MODE
+	{
+		if (MODE == '0') std::cout << "Training mode working";
+		else if (MODE == '1')
+		{
+			int num1{};
+			int num2{};
+			std::cout << "Combat mode working";
+			std::cout << std::endl;
+			std::cout << "Enter number 1: ";
+			std::cin >> num1;
+			std::cout << "Enter number 2: ";
+			std::cin >> num2;
+			std::cout << "Sum result: " << add(num1, num2);
+		}
+	} 
+#endif
+
+	return EXIT_SUCCESS;
+}
+
+int add(int a, int b)
+{
+	return a + b;
+}
