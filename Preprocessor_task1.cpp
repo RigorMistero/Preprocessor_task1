5c2f464a305ecda38c38e10c58f0a26a4cd6a7c0
Rigor Mistero <egor@student.su>
2025-09-14 12:09:41 +0300

    4th commit

--- Preprocessor_task1.cpp
+++ Preprocessor_task1.cpp
@@ -1,12 +1,9 @@
 ﻿#include <iostream>
 
-#define MODE 0
+#define MODE 3
 
-#if MODE == 1
-int add(int a, int b)
-	{
-		return a + b;
-	}
+#if MODE == 1 
+int add(int a, int b){	return a + b;}
 #endif
 
 int main(int argc, char** argv)
@@ -16,23 +13,24 @@ int main(int argc, char** argv)
 #error DEFINE MODE
 #endif 
 	
-#ifdef MODE
-	{
-		if (MODE == 0) std::cout << "Training mode working";
-		else if (MODE == 1)
-		{
-			int num1{};
-			int num2{};
+#if MODE == 0 
+	std::cout << "Training mode working" << std::endl;
+
+#elif MODE == 1
+
+			
 			std::cout << "Combat mode working";
 			std::cout << std::endl;
+			int num1{};
+			int num2{};
 			std::cout << "Enter number 1: ";
 			std::cin >> num1;
 			std::cout << "Enter number 2: ";
 			std::cin >> num2;
 			std::cout << "Sum result: " << add(num1, num2);
-		}
-		else { std::cout << "Unknown mode. Shutdown"; return 0; }
-	} 
+		
+#else	 
+	std::cout << "Unknown mode. Shutdown" << std::endl; 
 #endif
 
 	return EXIT_SUCCESS;
